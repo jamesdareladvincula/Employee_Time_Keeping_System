e7fb44555b44c3ff6d3fad54be95bbff5cce8148
James Darel Advincula <jdadvincula19@gmail.com>
2021-04-17 20:02:02 +0800

    commit message lezzgo

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Employee_Time_Keeping_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            string timeIn;
+            string timeOut;
+
+            Console.WriteLine("Employee Time Keeping System");
+			
+            Console.WriteLine("Enter your First Name:");
+			firstName = Console.ReadLine().ToUpper();
+            
+            if(firstName == "James")
+            {
+                 
+            }
+
+            else
+            {
+                while(firstName.Equals("f"))
+                {
+                    Console.WriteLine("Enter your First Name:");
+			        firstName = Console.ReadLine().ToUpper();
+                }
+            }
+
+            Console.WriteLine("Enter your Last Name:");
+			lastName = Console.ReadLine().ToUpper();
+			
+			Console.WriteLine("Enter Time-In:");
+			timeIn = Console.ReadLine();
+			
+			Console.WriteLine("Enter Time-Out:");
+			timeOut = Console.ReadLine();
+			
+			Console.WriteLine();
+
+            Console.WriteLine("Submitted Successfully!");
+			
+			Console.WriteLine("Employee Name:" + " " + lastName);
+			Console.WriteLine("Time-In:" + timeIn);
+			Console.WriteLine("Time-Out:" + timeOut);
+			
+			DateTime dateSubmitted = DateTime.Now;
+			Console.Write("Date Submitted:");
+			Console.WriteLine(dateSubmitted.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+        }
+    }
+}
