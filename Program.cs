169c7bcaca6c1b31aacd9e723abff366c8aa8ba8
James Darel Advincula <81962554+jamesdareladvincula@users.noreply.github.com>
2021-05-03 21:50:24 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,62 +6,62 @@ namespace Employee_Time_Keeping_System
     {
         static void Main(string[] args)
         {
-            string firstName;
-            string lastName;
-            string timeIn;
-            string timeOut;
-            DateTime timeInEntered;
-            DateTime timeOutEntered;
-            
-            Console.WriteLine("------------------------------------------------");
-            Console.WriteLine("|         Employee Time Keeping System         |");
-            Console.WriteLine("------------------------------------------------");
-			
-            Console.WriteLine("Enter your First Name:");
-			firstName = Console.ReadLine().ToUpper();
-            
-            Console.WriteLine("Enter your Last Name:");
-			lastName = Console.ReadLine().ToUpper();
-			
-			Console.WriteLine("Enter Time-In:");
-			timeIn = Console.ReadLine();
-			
-			Console.WriteLine("Enter Time-Out:");
-			timeOut = Console.ReadLine();
-			
+		string firstName;
+		    string lastName;
+		    string timeIn;
+		    string timeOut;
+		    DateTime timeInEntered;
+		    DateTime timeOutEntered;
+
+		    Console.WriteLine("------------------------------------------------");
+		    Console.WriteLine("|         Employee Time Keeping System         |");
+		    Console.WriteLine("------------------------------------------------");
+
+		    Console.WriteLine("Enter your First Name:");
+		    firstName = Console.ReadLine().ToUpper();
+
+		    Console.WriteLine("Enter your Last Name:");
+		    lastName = Console.ReadLine().ToUpper();
+
+		    Console.WriteLine("Enter Time-In:");
+		    timeIn = Console.ReadLine();
+
+		    Console.WriteLine("Enter Time-Out:");
+		    timeOut = Console.ReadLine();
+
+		    Console.WriteLine();
+
+		    Console.WriteLine("Submitted Successfully!");
+
+		    var validTimeIn = DateTime.TryParse(timeIn, out timeInEntered);
+
+		    var validTimeOut = DateTime.TryParse(timeOut, out timeOutEntered);
+
+		    if(firstName == "JAMES DAREL" && lastName == "ADVINCULA")
+		    {
+			Console.WriteLine("Employee Name:" + firstName + " " + lastName);
+
+			if(validTimeIn)
+			{
+			    Console.WriteLine("Time-In:" + timeInEntered.ToShortTimeString());
+			}
+
+			else if(validTimeOut)
+			{
+			    Console.WriteLine("Time-Out:" + timeOutEntered.ToShortTimeString());
+			}
+
+			else
+			{
+			    Console.WriteLine("Error! Invalid Time Format.");
+			}
+
 			Console.WriteLine();
 
-            Console.WriteLine("Submitted Successfully!");
-
-            var validTimeIn = DateTime.TryParse(timeIn, out timeInEntered);
-
-            var validTimeOut = DateTime.TryParse(timeOut, out timeOutEntered);
-			
-            if(firstName == "JAMES DAREL" && lastName == "ADVINCULA")
-            {
-			    Console.WriteLine("Employee Name:" + firstName + " " + lastName);
-			    
-                if(validTimeIn)
-                {
-                    Console.WriteLine("Time-In:" + timeInEntered.ToShortTimeString());
-                }
-
-                else if(validTimeOut)
-                {
-                    Console.WriteLine("Time-Out:" + timeOutEntered.ToShortTimeString());
-                }
-
-                else
-                {
-                    Console.WriteLine("Error! Invalid Time Format.");
-                }
-			    
-                Console.WriteLine();
-			
-			    DateTime dateSubmitted = DateTime.Now;
-			    Console.Write("Date Submitted:");
-			    Console.WriteLine(dateSubmitted.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
-            }
+			DateTime dateSubmitted = DateTime.Now;
+			Console.Write("Date Submitted:");
+			Console.WriteLine(dateSubmitted.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+		    }
         }
     }
 }
